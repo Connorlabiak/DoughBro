fb01549bd22442fab0cd9040afd5505cf3113681
Connor <connor_labiak@hotmail.com>
2026-06-17 23:53:48 -0400

    Created our first db model. Plan on creating db connection tomorrow.

---
+++ Models/TransactionDbModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DoughBro.Models
+{
+    public record class TransactionDbModel(string Id, string UserId, string Name, string Date, double Amount, string? Description, Category category)
+    {
+        string Id { get; set; }
+        string UserId { get; set; }
+        string Name { get; set; }
+        string Date { get; set; }
+        double Amount { get; set; }
+        string? Description { get; set; }
+        Category category { get; set; }
+    }
+}
