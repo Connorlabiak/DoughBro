2ba7987bfc47e1512944137555ce775c5a77b129
TheBigBuns27 <connor_labiak@hotmail.com>
2026-06-26 22:45:35 -0400

    Added a placeholder userModel

---
+++ Backend/src/Models/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using Google.Cloud.Firestore;
+
+namespace DoughBro.src.Models
+{
+	[FirestoreData]
+	public class UserModel
+	{
+		[FirestoreDocumentId]
+		public required string Id { get; set; }
+
+        [FirestoreProperty]
+		public required string FirstName { get; set; }
+
+        [FirestoreProperty]
+		public required string LastName { get; set; }
+
+		[FirestoreProperty]
+		public DateOnly? Birthday { get; set; }
+	}
+}
