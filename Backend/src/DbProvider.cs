a1d99dc02a9b24e1d691f08e6f1f9460fa31d021
Connor <connor_labiak@hotmail.com>
2026-07-01 00:30:20 -0400

    Updated DbProvider to work with both local and deployed versions.

--- Backend/src/DbProvider.cs
+++ Backend/src/DbProvider.cs
@@ -12,7 +12,7 @@ namespace DoughBro.src
     public class DbProvider: IDbProvider
     {
         private readonly FirestoreDb _db;
-        public DbProvider(IConfiguration config)
+        public DbProvider(IConfiguration config, IWebHostEnvironment environment)
         {
             IConfigurationSection firebaseSection = config.GetSection("Firebase");
             if (!firebaseSection.Exists())
@@ -22,15 +22,24 @@ namespace DoughBro.src
 
             string projectId = firebaseSection["project_id"] ?? throw new InvalidOperationException("Missing Firebase project_id from credential.");
 
-            var configDictionary = firebaseSection.AsEnumerable().ToDictionary(k => k.Key.Replace("Firebase:", ""), v => v.Value);
-            string serializedCredentials = JsonSerializer.Serialize(configDictionary);
-            var serviceAccountCredential = CredentialFactory.FromJson<ServiceAccountCredential>(serializedCredentials);
-
-            _db = new FirestoreDbBuilder
+            if (environment.IsDevelopment())
             {
-                ProjectId = projectId,
-                Credential = serviceAccountCredential.ToGoogleCredential()
-            }.Build();
+                var configDictionary = firebaseSection.AsEnumerable().ToDictionary(k => k.Key.Replace("Firebase:", ""), v => v.Value);
+                string serializedCredentials = JsonSerializer.Serialize(configDictionary);
+                var serviceAccountCredential = CredentialFactory.FromJson<ServiceAccountCredential>(serializedCredentials);
+
+                _db = new FirestoreDbBuilder
+                {
+                    ProjectId = projectId,
+                    Credential = serviceAccountCredential.ToGoogleCredential()
+                }.Build();
+            }
+            else
+            {
+                _db = FirestoreDb.Create(projectId);
+            }
+
+            
         }
 
         public FirestoreDb GetFirestoreDb()
